5bcabbee4bb281c207452fbc3009281b368f76f1
Aweber32 <78461462+Aweber32@users.noreply.github.com>
2021-02-02 23:19:12 -0500

    Add files via upload

---
+++ NBA/index.js
@@ -0,0 +1,49 @@
+
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidebar.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+  mySidebar.style.display = "none";
+  overlayBg.style.display = "none";
+}
+
+var countDownDate = new Date("May 10, 2021 15:37:25").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("gametimeclock").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+    if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("gametimeclock").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+function collect_data() {
+    var inname = document.getElementById("name").value;
+    var inpoints = document.getElementById("points").value;  
+}
+
+
